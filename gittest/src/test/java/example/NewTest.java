ab19328e8283951776428c0cceaa49f5e594358a
Shetty, Bharatraj <bharatrajs@gmail.com.com>
2017-06-25 21:10:51 +0530

    removed assert (in branch2) added sysout ****

--- gittest/src/test/java/example/NewTest.java
+++ gittest/src/test/java/example/NewTest.java
@@ -16,7 +16,7 @@ public class NewTest {
 	public void testEasy() {	
 	driver.get("http://demo.guru99.com/selenium/guru99home/");  
 	String title = driver.getTitle();				 
-	Assert.assertTrue(title.contains("Demo Guru99 Page")); 	
+	System.out.println("**************************************************************");
 	System.out.println("TEST 1");
 	System.out.println("TEST 2");
 }	
